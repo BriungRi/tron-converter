a03c7773b2d65fc56c111aa0f4b01c99578faf34
Brian Li <brnli7@gmail.com>
2025-07-13 16:55:53 -0400

    Add Github back in

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,50 @@
-import './App.css';
-import React, { useCallback, useState, useRef } from 'react';
-import { toHex, fromHex } from './utils';
-import { utils as ethersUtils } from 'ethers';
+import "./App.css";
+import React, { useCallback, useState, useRef } from "react";
+import { toHex, fromHex } from "./utils";
+import { utils as ethersUtils } from "ethers";
 
 function App(): JSX.Element {
-  const [hexAddress, setHexAddress] = useState<string>('');
-  const [tronAddress, setTronAddress] = useState<string>('');
+  const [hexAddress, setHexAddress] = useState<string>("");
+  const [tronAddress, setTronAddress] = useState<string>("");
 
   const tronInput = useRef<HTMLInputElement>(null);
   const hexInput = useRef<HTMLInputElement>(null);
 
-  const convertTronToHex = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const input = tronInput.current?.value ?? '';
-      if (input) {
-        const convertedHex = ethersUtils.getAddress(toHex(input).substring(2));
-        setHexAddress(convertedHex);
+  const convertTronToHex = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      try {
+        const input = tronInput.current?.value ?? "";
+        if (input) {
+          const convertedHex = ethersUtils.getAddress(
+            toHex(input).substring(2)
+          );
+          setHexAddress(convertedHex);
+        }
+      } catch (err) {
+        // eslint-disable-next-line no-alert -- simple demo error handling
+        alert(err);
       }
-    } catch (err) {
-      // eslint-disable-next-line no-alert -- simple demo error handling
-      alert(err);
-    }
-  }, []);
+    },
+    []
+  );
 
-  const convertHexToTron = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const input = hexInput.current?.value ?? '';
-      if (input) {
-        const convertedTron = fromHex(input);
-        setTronAddress(convertedTron);
+  const convertHexToTron = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      try {
+        const input = hexInput.current?.value ?? "";
+        if (input) {
+          const convertedTron = fromHex(input);
+          setTronAddress(convertedTron);
+        }
+      } catch (err) {
+        // eslint-disable-next-line no-alert -- simple demo error handling
+        alert(err);
       }
-    } catch (err) {
-      // eslint-disable-next-line no-alert -- simple demo error handling
-      alert(err);
-    }
-  }, []);
+    },
+    []
+  );
 
   return (
     <div className="App">
@@ -45,7 +53,12 @@ function App(): JSX.Element {
         <form onSubmit={convertTronToHex}>
           <h2>Tron =&gt; Hex</h2>
           <label htmlFor="tron-address">Enter Tron address</label>
-          <input id="tron-address" className="InputField" type="text" ref={tronInput} />
+          <input
+            id="tron-address"
+            className="InputField"
+            type="text"
+            ref={tronInput}
+          />
           <input className="InputButton" type="submit" value="Convert" />
           <p className="ConversionResult">
             Hex address: <strong>{hexAddress}</strong>
@@ -59,11 +72,16 @@ function App(): JSX.Element {
         <form onSubmit={convertHexToTron}>
           <h2>Hex =&gt; Tron</h2>
           <label htmlFor="hex-address">Enter Hex address</label>
-          <input id="hex-address" className="InputField" type="text" ref={hexInput} />
+          <input
+            id="hex-address"
+            className="InputField"
+            type="text"
+            ref={hexInput}
+          />
           <input className="InputButton" type="submit" value="Convert" />
           <p className="ConversionResult">
             Tron address: <strong>{tronAddress} </strong>
-            {tronAddress !== '' && (
+            {tronAddress !== "" && (
               <a
                 target="_blank"
                 rel="noopener noreferrer"
@@ -74,6 +92,19 @@ function App(): JSX.Element {
             )}
           </p>
         </form>
+      </div>
+
+      <div className="Footer">
+        <p>
+          View source on{" "}
+          <a
+            href="https://github.com/BriungRi/tron-converter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
       </div>
     </div>
   );
